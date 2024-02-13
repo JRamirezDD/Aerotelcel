ba14e6514e76facf42df01bb3e0017116f8b2af8
Jorge Ramirez <jorge.ramirez1787@gmail.com>
2024-02-13 23:00:54 +0100

    n

---
+++ Services/reports_handler/src/main/java/com/reports_handler/controller/ReportsHandlerController.java
@@ -0,0 +1,16 @@
+package com.reports_handler.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/reports-handler")
+@RequiredArgsConstructor
+@Slf4j
+public class ReportsHandlerController {
+    // Empty path class.
+        // General methods can be added here.
+}
--- Services/subscription_handler/src/main/java/com/subscription_handler/Service/SubscriptionHandlerService.java
+++ Services/subscription_handler/src/main/java/com/subscription_handler/Service/SubscriptionHandlerService.java
@@ -5,10 +5,8 @@ import com.subscription_redis.dto.SubscriptionResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.util.UriComponentsBuilder;
 
