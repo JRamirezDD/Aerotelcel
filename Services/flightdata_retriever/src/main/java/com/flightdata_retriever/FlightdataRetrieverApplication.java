a0f1a0a2d6a9e59f7545880738e59790ec36b20b
javsort <javierortega336@gmail.com>
2024-02-21 11:57:27 +0100

    Added flightdata_retriever skeleton

---
+++ Services/flightdata_retriever/src/main/java/com/flightdata_retriever/FlightdataRetrieverApplication.java
@@ -0,0 +1,13 @@
+package com.flightdata_retriever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightdataRetrieverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightdataRetrieverApplication.class, args);
+	}
+
+}
