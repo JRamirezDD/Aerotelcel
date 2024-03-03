fe3a03e0cd4ee6c3ecb94089e86ac11bec57f114
Jorge Ramirez <jramirezdediego@gmail.com>
2024-03-03 10:18:13 +0100

    idk

--- Services/flightdata_handler/src/main/java/com/flightdata_handler/controller/AirportDataController.java
+++ Services/flightdata_handler/src/main/java/com/flightdata_handler/controller/AirportDataController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/airportDataController")
+@RequestMapping("/api/airportDataController")
 @Slf4j
 public class AirportDataController {
 
--- Services/flightdata_handler/src/main/java/com/flightdata_handler/controller/FlightDataController.java
+++ Services/flightdata_handler/src/main/java/com/flightdata_handler/controller/FlightDataController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/flightController")
+@RequestMapping("/api/flightController")
 @Slf4j
 public class FlightDataController {
 
